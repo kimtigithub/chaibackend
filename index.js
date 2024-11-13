6328a975566f07bd0b81975185962c8fdbc6f055
kimti github <kanvisark@gmail.com>
2024-11-13 18:23:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!') //callback , res = response
+})
+
+app.get('/twitter',(req, res)=>{
+res.send("kimti@gmail.com")
+})
+
+app.get('/login',(req, res) =>{            //'/login' is a route
+res.send('<h1>please login at chai aur code</h1>')
+})
+
+app.get('/kimti',(req, res)=>{
+res.send('Hi This is Kimti a bloody lower')
+})
+
+app.get('/chai', (req,res) => {
+res.send("Hi ")
+})
+
+app.get('/youTube',(req,res) => {
+res.send("Chai aur code")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
